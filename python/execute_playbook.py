b09fcd5f097054d9cd22d4f5482a07a8de0b3f05
wael <r0781669@student.thomasmore.be>
2023-05-18 20:19:41 +0000

    ssh keys aanmaken en gebruiken done

--- python/execute_playbook.py
+++ python/execute_playbook.py
@@ -1,7 +1,30 @@
+# import os
+# import subprocess
+
+# def execute_playbook(playbook_path, extra_vars=None):
+#     # Change to the directory containing the playbook
+#     playbook_directory = os.path.dirname(playbook_path)
+#     os.chdir(playbook_directory)
+
+#     # Construct the ansible-playbook command
+#     command = ['ansible-playbook', playbook_path]
+#     if extra_vars:
+#         command.extend(['-e', extra_vars])
+
+#     # Execute the ansible-playbook command
+#     subprocess.run(command, check=True)
+
+# # Example usage
+# playbook_path = '/home/wael/project_hosting/ansible-playbook/playbook.yaml'
+# extra_vars = "varA=okey varB=newValue"
+# execute_playbook(playbook_path, extra_vars)
+
+
+
 import os
 import subprocess
 
-def execute_playbook(playbook_path, extra_vars=None):
+def execute_playbook(playbook_path, extra_vars=None, password=None):
     # Change to the directory containing the playbook
     playbook_directory = os.path.dirname(playbook_path)
     os.chdir(playbook_directory)
@@ -10,6 +33,8 @@ def execute_playbook(playbook_path, extra_vars=None):
     command = ['ansible-playbook', playbook_path]
     if extra_vars:
         command.extend(['-e', extra_vars])
+    if password:
+        command.extend(['--extra-vars', f'"ansible_ssh_pass={password}"'])
 
     # Execute the ansible-playbook command
     subprocess.run(command, check=True)
@@ -17,4 +42,5 @@ def execute_playbook(playbook_path, extra_vars=None):
 # Example usage
 playbook_path = '/home/wael/project_hosting/ansible-playbook/playbook.yaml'
 extra_vars = "varA=okey varB=newValue"
-execute_playbook(playbook_path, extra_vars)
+password = "ansible"  # Replace with your actual password
+execute_playbook(playbook_path, extra_vars, password)
